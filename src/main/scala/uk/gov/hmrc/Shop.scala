9cd769b8ff26a8feecce93b6f66c1f9f0e400e4d
Abimbola Esuruoso <abimbolaesuruoso@gmail.com>
2017-02-24 11:15:17 +0000

    Added the implementation for the discount feature

--- src/main/scala/uk/gov/hmrc/Shop.scala
+++ src/main/scala/uk/gov/hmrc/Shop.scala
@@ -9,6 +9,11 @@ object Shop {
     "apple" -> BigDecimal(0.6),
     "orange" -> BigDecimal(0.25)
   )
+  
+  val discountFactor = Map(
+    "apple" -> 2,
+    "orange" -> 3
+  )
 
   def main(args: Array[String]): Unit = {
     val cost = purchase(args)
@@ -18,11 +23,21 @@ object Shop {
     println(receipt)
   }
 
-  def purchase(basket: Seq[String]): BigDecimal = costOf(basket)
+  def purchase(basket: Seq[String]): BigDecimal = costOf(basket) - discountOf(basket)
 
-  private def costOf(items: Seq[String]) = {
+  private def costOf(items: Seq[String]): BigDecimal = {
     items
       .flatMap(catalogue.get)
       .sum
+  }
+
+  private def discountOf(items: Seq[String]): BigDecimal = {
+    val (apples, oranges) = items.sorted.partition(_ == "apple")
+
+    val totalAppleDiscount = apples.size / discountFactor("apple") * catalogue("apple")
+    val totalOrangeDiscount = oranges.size / discountFactor("orange") * catalogue("orange")
+
+    val totalDiscount = totalAppleDiscount + totalOrangeDiscount
+    totalDiscount
   }
 }
